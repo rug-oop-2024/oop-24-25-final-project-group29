0096227bd7a8997b90a9586ebd930c0f394431db
divan <hegeman.ivan@gmail.com>
2024-11-08 20:40:21 +0100

    added slectbox for target and input features

--- app/pages/1_📊_Datasets.py
+++ app/pages/1_📊_Datasets.py
@@ -7,24 +7,30 @@ from autoop.core.ml.dataset import Dataset
 automl = AutoMLSystem.get_instance()
 
 datasets = automl.registry.list(type="dataset")
+dataset_display_names = [artifact.name for artifact in datasets]
 
 
-def app():
-    st.title("Datasets")
-    st.write("Here the user can manage the datasets.")
+st.title("Datasets")
+st.write("Here the user can manage the datasets.")
 
-    uploaded_file = st.file_uploader('Choose a csv file', type='csv')
+name = st.selectbox(
+    'Select a dataset',
+    dataset_display_names
+    )
+if name:
+    dataset = datasets[dataset_display_names.index(name)]
 
+uploaded_file = st.file_uploader('Choose a csv file', type='csv')
+
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+    asset_path = f"datasets/{uploaded_file.name}"
+    dataset = Dataset.from_dataframe(
+        name=uploaded_file.name,
+        data=df,
+        asset_path=asset_path
+    )
     if st.button("Add dataset"):
-        name = uploaded_file.name # REMOVE .csv
-        st.write(name)
-        df = pd.read_csv(uploaded_file)
-        st.write(df)
-        asset_path = f"datasets/{uploaded_file.name}"
-        st.write(asset_path)
-
-        dataset = Dataset.from_dataframe()
         automl.registry.register(dataset)
-
-
-app()
+        st.success("Dataset added!")
+        st.rerun()
