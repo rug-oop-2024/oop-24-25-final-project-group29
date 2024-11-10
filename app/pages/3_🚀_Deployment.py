e2a0877c7a3a1ff1d8123215ad800b666d761e9a
setosetoseto <d.s.setoain@student.rug.nl>
2024-11-10 20:35:10 +0100

    layout improvenment

--- app/pages/3_🚀_Deployment.py
+++ app/pages/3_🚀_Deployment.py
@@ -44,93 +44,83 @@ automl = AutoMLSystem.get_instance()
 st.title("Deployment")
 st.write("Here the user can manage the pipelines.")
 
-pipeline_artifacts = automl.registry.list(type="pipeline")
+# Section for managing pipelines
+with st.expander("Manage Pipelines", expanded=True):
+    pipeline_artifacts = automl.registry.list(type="pipeline")
+    pipeline_names = [artifact.name for artifact in pipeline_artifacts]
 
-pipeline_names = [artifact.name for artifact in pipeline_artifacts]
-
-selected_pipeline_names = st.multiselect(
-    'Select pipelines to manage',
-    pipeline_names
+    selected_pipeline_names = st.multiselect(
+        'Select pipelines to manage',
+        pipeline_names
     )
-if selected_pipeline_names:
-    selected_pipelines = []
-    for name in selected_pipeline_names:
-        selected_pipelines.append(
-            pipeline_artifacts[pipeline_names.index(name)]
-            )
 
-    col1, col2 = st.columns(2)
-    view_button = False
-    delete_button = False
-    with col1:
-        if st.button("View Pipelines"):
-            view_button = True
-    with col2:
-        if st.button("Delete Selected Pipelines"):
-            delete_button = True
+    if selected_pipeline_names:
+        selected_pipelines = [pipeline_artifacts[pipeline_names.index(name)] for name in selected_pipeline_names]
 
-    if view_button:
-        for pipeline_artifact in selected_pipelines:
-            pipeline_config = _get_pipeline_config(pipeline_artifact)
-            if pipeline_config:
-                st.subheader(f"Pipeline: {pipeline_artifact.name}")
-                st.write(
-                    f"Input Features: {', '.join(pipeline_config[
+        col1, col2 = st.columns(2)
+        view_button = col1.button("View Pipelines")
+        delete_button = col2.button("Delete Selected Pipelines")
+
+        if view_button:
+            for pipeline_artifact in selected_pipelines:
+                pipeline_config = _get_pipeline_config(pipeline_artifact)
+                if pipeline_config:
+                    st.subheader(f"Pipeline: {pipeline_artifact.name}")
+                    st.write(f"Input Features: {', '.join(pipeline_config[
                         'input_features'
-                        ])}"
+                        ])}")
+                    st.write(f"Target Feature: {pipeline_config[
+                        'target_feature'
+                        ]}")
+                    st.write(f"Train/Test Split: {pipeline_config['split']}")
+
+        if delete_button:
+            for current in selected_pipelines:
+                automl.registry.delete(current.id)
+            st.success("Pipelines(s) deleted!")
+
+with st.expander("Load Pipeline for Predictions", expanded=True):
+    load_pipeline_name = st.selectbox(
+        'Select a pipeline for performing predictions', pipeline_names
+        )
+
+    if load_pipeline_name:
+        load_pipeline = pipeline_artifacts[
+            pipeline_names.index(load_pipeline_name)
+            ]
+        model = _get_pipeline_model(load_pipeline)
+        pipeline_config = _get_pipeline_config(load_pipeline)
+
+        uploaded_file = st.file_uploader('Choose a CSV file', type='csv')
+
+        if st.button("Load Pipeline"):
+            if uploaded_file:
+                df = pd.read_csv(uploaded_file)
+                asset_path = f"datasets/{uploaded_file.name}"
+                dataset = Dataset.from_dataframe(
+                    name=uploaded_file.name,
+                    data=df,
+                    asset_path=asset_path
+                )
+
+                input_feature_names = pipeline_config["input_features"]
+                input_data = df[input_feature_names]
+                y = df[pipeline_config["target_feature"]]
+
+                # Fit the model and make predictions
+                model.fit(input_data, y)
+                predictions = model.predict(input_data)
+
+                st.write("Predictions:")
+                prediction_df = pd.DataFrame(
+                    predictions, columns=[pipeline_config["target_feature"]]
                     )
-                st.write(f"Target Feature: {pipeline_config[
-                    'target_feature'
-                    ]}")
+                st.table(prediction_df.head(10))
 
-                st.write(f"Train/Test Split: {pipeline_config['split']}")
-
-    if delete_button:
-        for current in selected_pipelines:
-            automl.registry.delete(current.id)
-        st.success("Pipelines(s) deleted!")
-
-load_pipeline_name = st.selectbox(
-    'Select a pipeline for performing predictions',
-    pipeline_names
-)
-if load_pipeline_name:
-    load_pipeline = pipeline_artifacts[
-        pipeline_names.index(load_pipeline_name)
-        ]
-    model = _get_pipeline_model(load_pipeline)
-    pipeline_config = _get_pipeline_config(load_pipeline)
-
-    uploaded_file = st.file_uploader('Choose a csv file', type='csv')
-
-    if st.button("Load Pipeline"):
-        if uploaded_file:
-            df = pd.read_csv(uploaded_file)
-            asset_path = f"datasets/{uploaded_file.name}"
-            dataset = Dataset.from_dataframe(
-                name=uploaded_file.name,
-                data=df,
-                asset_path=asset_path
-            )
-
-            input_feature_names = pipeline_config["input_features"]
-            input_data = df[input_feature_names]
-            y = df[pipeline_config["target_feature"]]
-
-            # Fit the model and make predictions
-            model.fit(input_data, y)
-            predictions = model.predict(input_data)
-
-            st.write("Predictions:")
-            prediction_df = pd.DataFrame(predictions, columns=[
-                pipeline_config["target_feature"]
-                ])
-            st.table(prediction_df.head(10))
-
-            st.download_button(
-                label="Download Predictions CSV",
-                data=prediction_df.to_csv(index=False),
-                file_name="predictions.csv",
-                mime="text/csv",
-                key="download_predictions_csv"
-            )
+                st.download_button(
+                    label="Download Predictions CSV",
+                    data=prediction_df.to_csv(index=False),
+                    file_name="predictions.csv",
+                    mime="text/csv",
+                    key="download_predictions_csv"
+                )
